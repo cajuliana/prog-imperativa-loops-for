cf28bc94d4270130d206154535fff87077464266
Carla Oliveira <89038203+cajuliana@users.noreply.github.com>
2021-09-30 16:13:32 -0300

    Add files via upload

---
+++ loops-for.js
@@ -0,0 +1,14 @@
+for (let i = 0; i <= 4; i++){
+    console.log("Olá, mundo!");
+}
+
+for (let i = 1; i <= 10; i++){
+    if (i % 2 != 0) {
+        console.log(i);
+    }
+}
+
+for (let i = 0; i <= 10; i++){
+    console.log("7 x " + i + "=" + 7*i);}
+
+    
